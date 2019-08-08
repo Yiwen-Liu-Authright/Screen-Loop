fbbfbf0dcd279a3ab5dbd13b34601f5163a5d8f9
Yiwen-Liu-Authright <yiwen.liu@authright.com>
2019-08-08 13:05:26 -0400

    Install redux + react-redux + redux-devtools

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 //import logo from './logo.svg';
 import ScreenLoop from './containers/ScreenLoop';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 //import './App.css';
+
+// Initial props
+const props = {
+  optionList: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFEicmlVjxOcfOE_A0quf9A2EPPiN8QWZkIGG1ZLyU1_KagfMG",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6EnskTIdRmctYiQbwUKVvInjUfWPk9p3-OOCu-IutovROIT19",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4Am1yp6K7oLi_bcVU72ALVwsG_xF9MJsoH9V0a4IKK-8jksLw",
+    "http://cms-bucket.ws.126.net/2019/04/02/6b2f4f2690e744b48deb7ee164faee38.jpeg"],
+  currentInterval: undefined,
+}
+
+// Add Reducer
+function reducer() {
+  return {...props}
+}
+
+// Add Store
+const store = createStore(reducer);
 
 function App() {
   return (
     <div className="App">
       <div className="jumbotron jumbotron-fluid" style={{ marginTop: "100px" }}>
         <div className="container">
-          <ScreenLoop />
+          {/* Add Provider */}
+          <Provider store={store}>
+            <ScreenLoop {...props} />
+          </Provider>
         </div>
       </div>
     </div>
--- src/containers/ScreenLoop.js
+++ src/containers/ScreenLoop.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 import Title from '../components/Title';
 import UrlBox from './UrlBox';
 import Aux from '../hoc/Aux/Aux';
@@ -6,20 +7,20 @@ import ImageDisplay from './ImageDisplay';
 
 
 class ScreenLoop extends Component {
-    state = {
-        //苏大强
-        // optionList: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFEicmlVjxOcfOE_A0quf9A2EPPiN8QWZkIGG1ZLyU1_KagfMG",
-        //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6EnskTIdRmctYiQbwUKVvInjUfWPk9p3-OOCu-IutovROIT19",
-        //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4Am1yp6K7oLi_bcVU72ALVwsG_xF9MJsoH9V0a4IKK-8jksLw",
-        //     "http://cms-bucket.ws.126.net/2019/04/02/6b2f4f2690e744b48deb7ee164faee38.jpeg"],
-        //猫片
-        optionList: ["//placekitten.com/1500/500",
-                "//placekitten.com/4000/3000",
-                "//placekitten.com/800/1200",
-                "//placekitten.com/1500/1500"],
-        currentInterval: undefined,
-        // loop: undefined,
-    };
+    // state = {
+    //     //苏大强
+    //     // optionList: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFEicmlVjxOcfOE_A0quf9A2EPPiN8QWZkIGG1ZLyU1_KagfMG",
+    //     //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6EnskTIdRmctYiQbwUKVvInjUfWPk9p3-OOCu-IutovROIT19",
+    //     //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4Am1yp6K7oLi_bcVU72ALVwsG_xF9MJsoH9V0a4IKK-8jksLw",
+    //     //     "http://cms-bucket.ws.126.net/2019/04/02/6b2f4f2690e744b48deb7ee164faee38.jpeg"],
+    //     //猫片
+    //     optionList: ["//placekitten.com/1500/500",
+    //             "//placekitten.com/4000/3000",
+    //             "//placekitten.com/800/1200",
+    //             "//placekitten.com/1500/1500"],
+    //     currentInterval: undefined,
+    //     // loop: undefined,
+    // };
 
     currentIntervalHandler = (value) => {
         this.setState({ currentInterval: value })
@@ -40,14 +41,14 @@ class ScreenLoop extends Component {
                 <Title headerTitle="Funny Screen" comments="A list of pages to be looped." />
                 <UrlBox
                     optionListHandler={this.optionListHandler}
-                    optionList={this.state.optionList}
+                    optionList={this.props.optionList}
                     currentIntervalHandler={this.currentIntervalHandler}
-                    currentInterval={this.state.currentInterval} />
+                    currentInterval={this.props.currentInterval} />
                 <ImageDisplay
-                    optionList={this.state.optionList}
+                    optionList={this.props.optionList}
                     // loop={this.state.loop}
                     // loopHandler={this.loopHandler}
-                    currentInterval={this.state.currentInterval}
+                    currentInterval={this.props.currentInterval}
                 />
                 {/* {console.log("Option List: " + this.state.optionList)}
                 {console.log("CurrentInterval: " + this.state.currentInterval)} */}
@@ -58,4 +59,8 @@ class ScreenLoop extends Component {
     }
 }
 
-export default ScreenLoop;
+const mapStateToProps = (state) => ({
+    optionList: state.optionList,
+})
+// connect is a hoc function, it takes a function as parameter and return the function
+export default connect(mapStateToProps)(ScreenLoop);
